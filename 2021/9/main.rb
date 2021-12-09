454f974852879334ac11248e84d3345d6394c8d6
C.J.Kinniburgh <chris@cjkinni.com>
2021-12-09 07:59:55 -0600

    Refactor 2021 Day 9

--- 2021/9/main.rb
+++ 2021/9/main.rb
@@ -1,38 +1,26 @@
-# frozen_string_literal: true
+require 'set'
+require 'powertools'
 
 lines = File.read('input').split("\n")
 grid = lines.map { |l| l.split('').map(&:to_i) }
 
 low_points = []
 grid.each_with_index do |row, y|
-  row.each_with_index do |cell, x|
-    next unless (x.zero? || cell < row[x - 1]) &&
-                (x == row.count - 1 || cell < row[x + 1]) &&
-                (y.zero? || cell < grid[y - 1][x]) &&
-                (y == grid.count - 1 || cell < grid[y + 1][x])
+  row.each_with_index do |value, x|
+    next unless (x.zero? || value < row[x - 1]) &&
+                (x == row.count - 1 || value < row[x + 1]) &&
+                (y.zero? || value < grid[y - 1][x]) &&
+                (y == grid.count - 1 || value < grid[y + 1][x])
 
-    low_points << cell
+    low_points << [y, x, value]
   end
 end
 
-puts "1: #{low_points.map { |i| i + 1 }.sum}"
-
-low_points = []
-grid.each_with_index do |row, y|
-  row.each_with_index do |cell, x|
-    next unless (x.zero? || cell < row[x - 1]) &&
-                (x == row.count - 1 || cell < row[x + 1]) &&
-                (y.zero? || cell < grid[y - 1][x]) &&
-                (y == grid.count - 1 || cell < grid[y + 1][x])
-
-    low_points << [y.clone, x.clone]
-  end
-end
+puts "1: #{low_points.map { |_x, _y, v| v + 1 }.sum}"
 
 basin_queues = []
-low_points.each_with_index do |lp, _lp_i|
-  ly, lx = lp
-  basin_queues << [[ly, lx, grid[ly][lx]]]
+low_points.each_with_index do |lp|
+  basin_queues << Set.new([lp])
 end
 
 xr = (0...grid.first.length)
@@ -42,18 +30,14 @@ basin_queues.each_with_index do |_, qi|
   while basin_queues[qi].count.positive?
     ly, lx, lv = basin_queues[qi].pop
     [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |dy, dx|
-      x = lx + dx
-      y = ly + dy
-      next unless xr.include?(x) && yr.include?(y)
-      next unless grid[y][x] < 9
-
-      basin_cells[qi] = [[ly, lx, lv]] if basin_cells[qi].nil?
+      x, y = (lx+dx), (ly+dy)
+      next unless xr.include?(x) && yr.include?(y) && grid[y][x] < 9
+      basin_cells[qi] = Set.new([[ly, lx, lv]]) if basin_cells[qi].nil?
 
       v = grid[y][x]
       result = [y, x, v]
-      if v >= lv && v != 9 && !basin_cells[qi].include?(result)
-        basin_cells[qi] << result
-        basin_queues[qi] << result
+      if v >= lv && v != 9
+        basin_queues[qi].add(result) if basin_cells[qi].add?(result)
       end
     end
   end

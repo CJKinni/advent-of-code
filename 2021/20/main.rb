8c92cbca8d4c57a987ea53e250680aaad7bf30df
C.J.Kinniburgh <chris@cjkinni.com>
2021-12-20 00:43:35 -0600

    Refactor

--- 2021/20/main.rb
+++ 2021/20/main.rb
@@ -1,9 +1,3 @@
-require 'set'
-
-lines = File.read('input').split("\n")
-
-enhancement = lines[0].split('').map { |c| c == '#' }
-
 def get_relevant_pixels(original, y, x)
   pixels = []
   (y - 1..y + 1).each do |ry|
@@ -37,15 +31,6 @@ def enhance(enhancement, canvas, light_pixels)
   light_pixels_new
 end
 
-grid = lines[2..].map { |l| l.split('') }
-
-light_pixels_original = Set.new
-grid.each_with_index do |row, y|
-  row.each_with_index do |cell, x|
-    light_pixels_original << [y, x] if cell == '#'
-  end
-end
-
 def draw(light_pixels)
   min_x, max_x, min_y, max_y =  get_dimensions(light_pixels)
   (min_y..max_y).each do |y|
@@ -68,35 +53,43 @@ def crop(canvas, light_pixels)
   end
 end
 
-light_pixels = light_pixels_original
-min_x, max_x, min_y, max_y =  get_dimensions(light_pixels)
-canvas = [min_x - 30, max_x + 30, min_y - 30, max_y + 30]
-
-(1..2).each do |_i|
-  light_pixels = enhance(enhancement, canvas, light_pixels)
-end
-
-canvas = [min_x - 10, max_x + 10, min_y - 10, max_y + 10]
-lp = crop(canvas, light_pixels)
-
-puts "1: #{lp.count}"
-
-light_pixels = light_pixels_original
-min_x, max_x, min_y, max_y =  get_dimensions(light_pixels)
-canvas = [min_x - 30, max_x + 30, min_y - 30, max_y + 30]
-
-(1..50).each do |i|
-  if i.odd?
+# This assumes a '#' as the first enhance bit and '.' as the last.
+def iterate(light_pixels, enhancement, count)
     min_x, max_x, min_y, max_y =  get_dimensions(light_pixels)
-    canvas = [min_x - 30, max_x + 30, min_y - 30, max_y + 30]
-  end
-
-  light_pixels = enhance(enhancement, canvas, light_pixels)
-
-  next unless i.even?
-  canvas = [min_x - 10, max_x + 10, min_y - 10, max_y + 10]
-  light_pixels = crop(canvas, light_pixels)
+    canvas = nil
+    (1..count).each do |i|
+        if i.odd?
+          min_x, max_x, min_y, max_y =  get_dimensions(light_pixels)
+          canvas = [min_x - 3, max_x + 3, min_y - 3, max_y + 3]
+        end
+      
+        light_pixels = enhance(enhancement, canvas, light_pixels)
+      
+        next unless i.even?
+        canvas = [min_x - 2, max_x + 2, min_y - 2, max_y + 2]
+        light_pixels = crop(canvas, light_pixels)
+      end
+      light_pixels      
 end
 
-# Solves in ~10s
+require 'set'
+
+lines = File.read('input').split("\n")
+
+enhancement = lines[0].split('').map { |c| c == '#' }
+
+grid = lines[2..].map { |l| l.split('') }
+
+light_pixels_original = Set.new
+grid.each_with_index do |row, y|
+  row.each_with_index do |cell, x|
+    light_pixels_original << [y, x] if cell == '#'
+  end
+end
+
+light_pixels = iterate(light_pixels_original, enhancement, 2)
+puts "1: #{light_pixels.count}"
+
+# Solves in ~9s
+light_pixels = iterate(light_pixels_original, enhancement, 50)
 puts "2: #{light_pixels.count}"

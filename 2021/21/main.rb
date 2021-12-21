426e2a4f31fa10c92127918c6b7e01e28fac588c
C.J.Kinniburgh <chris@cjkinni.com>
2021-12-21 08:24:53 -0600

    Refactor Day 21

--- 2021/21/main.rb
+++ 2021/21/main.rb
@@ -1,12 +1,12 @@
-class GameState
-  attr_accessor :pos, :last_player, :score, :die
+class BasicGame
+  attr_reader :score
 
-  def initialize(p1_start, p2_start, die, final_score)
+  def initialize(p1_start, p2_start, die)
     @pos = [p1_start, p2_start]
     @last_player = 1
     @score = [0, 0]
     @die = die
-    @final_score = final_score
+    @final_score = 1000
   end
 
   def next_player
@@ -15,7 +15,7 @@ class GameState
 
   def take_turn
     player = next_player
-    rolls = (0..2).map { |_| @die.roll }.reduce(&:+)
+    rolls = (0..2).map { |_| @die.roll }.sum
     @pos[player] = ((@pos[player] + rolls) % 10)
     @pos[player] = 10 if (@pos[player]).zero?
     @score[player] += @pos[player]
@@ -42,20 +42,24 @@ class DeterministicDie
 end
 
 die = DeterministicDie.new
-
-game = GameState.new(6, 3, die, 1000)
+game = BasicGame.new(6, 3, die)
 
 game.take_turn until game.over?
+
 puts "1: #{die.count * game.score.min}"
 
 
 class DiracGame
   def initialize
+    # Moves is a hash with one item for each of the possible outcomes of 3
+    # universe-splitting rolls of a d3, mapped to a count of the occurrences.
     choices = [1, 2, 3]
-    @moves = choices.product(choices).product(choices).map { |arr| arr.flatten.reduce(&:+) }
+    @moves = choices.product(choices, choices).map(&:sum).tally
     @cache = {}
   end
 
+  # Memoized DFS, playing out each game in each universe until we get a winner.
+  # We return an array with a count of the wins for each player.
   def play_games(turn, score, tile)
     return @cache[[turn, score, tile]] if @cache[[turn, score, tile]]
 
@@ -63,7 +67,7 @@ class DiracGame
       return score[0] >= 21 ? [1,0] : [0,1]
     end
 
-    wins = @moves.map do |move|
+    wins = @moves.map { |move, count|
       itile, iscore = tile.clone, score.clone
       next_turn = turn.zero? ? 1 : 0
 
@@ -71,10 +75,10 @@ class DiracGame
       itile[turn] = 10 if itile[turn] == 0
 
       iscore[turn] += itile[turn]
-      play_games(next_turn, iscore, itile)
-    end
+      [play_games(next_turn, iscore, itile)] * count
+    }.reduce(&:+)
 
-    @cache[[turn, score, tile]] = wins.transpose.map { |arr| arr.reduce(&:+) }
+    @cache[[turn, score, tile]] = wins.transpose.map(&:sum)
   end
 end
 

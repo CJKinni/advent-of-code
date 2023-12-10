fe7dc4e002d6d4b58bf072c3e069c8ee8ae9b57e
C.J.Kinniburgh <chris@cjkinni.com>
2023-12-10 08:16:39 -0600

    2023 Day 10 - Some Memory Research

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -1,4 +1,6 @@
-from collections import deque
+import math
+import re
+from bitarray import bitarray
 
 input = open("input.txt", "r")
 lines = input.read().splitlines()
@@ -73,55 +75,56 @@ while links != []:
 
 print(f"1: {distance-1}")
 
-for link in links:
-    breakpoint()
-
 def build_big_nodes(node_lines):
+    # Make each area way bigger to make this easy to calculate:
     new_lines=[]
     for node_line in node_lines:
-        new_lines.append([])
-        new_lines.append([])
-        new_lines.append([])
+        new_lines.append(bitarray())
+        new_lines.append(bitarray())
+        new_lines.append(bitarray())
         for node in node_line:
             if node.distance == None:
                 node.char = '.'
             if node.char == '|':
-                new_lines[-3] += [False, True , False]
-                new_lines[-2] += [False, True , False]
-                new_lines[-1] += [False, True , False]
+                new_lines[-3] += bitarray('010')
+                new_lines[-2] += bitarray('010')
+                new_lines[-1] += bitarray('010')
             elif node.char == '-':
-                new_lines[-3] += [False, False, False]
-                new_lines[-2] += [True , True , True ]
-                new_lines[-1] += [False, False, False]
+                new_lines[-3] += bitarray('000')
+                new_lines[-2] += bitarray('111')
+                new_lines[-1] += bitarray('000')
             elif node.char == 'L':
-                new_lines[-3] += [False, True , False]
-                new_lines[-2] += [False, True , True ]
-                new_lines[-1] += [False, False, False]
+                new_lines[-3] += bitarray('010')
+                new_lines[-2] += bitarray('011')
+                new_lines[-1] += bitarray('000')
             elif node.char == 'J':
-                new_lines[-3] += [False, True , False]
-                new_lines[-2] += [True , True , False]
-                new_lines[-1] += [False, False, False]
+                new_lines[-3] += bitarray('010')
+                new_lines[-2] += bitarray('110')
+                new_lines[-1] += bitarray('000')
             elif node.char == '7':
-                new_lines[-3] += [False, False, False]
-                new_lines[-2] += [True , True , False]
-                new_lines[-1] += [False, True , False]
+                new_lines[-3] += bitarray('000')
+                new_lines[-2] += bitarray('110')
+                new_lines[-1] += bitarray('010')
             elif node.char == 'F':
-                new_lines[-3] += [False, False, False]
-                new_lines[-2] += [False, True , True ]
-                new_lines[-1] += [False, True , False]
+                new_lines[-3] += bitarray('000')
+                new_lines[-2] += bitarray('011')
+                new_lines[-1] += bitarray('010')
             elif node.char == '.':
-                new_lines[-3] += [False, False, False]
-                new_lines[-2] += [False, False, False]
-                new_lines[-1] += [False, False, False]
-            elif node.char == 'S': # A + works in all configurations!
-                new_lines[-3] += [False, True , False]
-                new_lines[-2] += [True , True , True ]
-                new_lines[-1] += [False, True , False]
+                new_lines[-3] += bitarray('000')
+                new_lines[-2] += bitarray('000')
+                new_lines[-1] += bitarray('000')
+            elif node.char == 'S':
+                new_lines[-3] += bitarray('010')
+                new_lines[-2] += bitarray('111')
+                new_lines[-1] += bitarray('010')
+            else:
+                breakpoint()
     
     return new_lines
 
 big_map = build_big_nodes(nodes)
 
+from collections import deque
 
 def flood_fill(pos, map, seen):
     queue = deque([pos])
@@ -136,7 +139,7 @@ def flood_fill(pos, map, seen):
             seen.add('EDGE')
             return seen
 
-        if map[y][x]:
+        if map[y][x] != 0:
             continue
 
         seen.add(current_pos)
@@ -160,7 +163,7 @@ for y, node_line in enumerate(nodes):
             continue
         if node.char != '.' and node.distance != None:
             continue
-        if big_map[y*3+1][x*3+1]:
+        if big_map[y*3+1][x*3+1] == 1:
             continue
         flood_resp = flood_fill(((x*3+1), (y*3+1)), big_map, set())
         if 'EDGE' not in flood_resp:
@@ -169,3 +172,61 @@ for y, node_line in enumerate(nodes):
             edge_adjacent = edge_adjacent.union(flood_resp)
 
 print(f"2: {len(squares)}")
+
+# This is some code I found (NOT MINE!) 
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
+total_size_nodes = total_size(nodes) # 1115640
+total_size_lines = total_size(lines) # 26580
+total_size_map = total_size(big_map)
+# 1572880 with the non-bitarray version
+# 60824 with the bitarray version
+
+
+breakpoint()
